5082a920cde37d0032b5c51748273d600581e51e
Alexandru Lucaci B2 <lucacialexandru18@gmail.com>
2022-04-13 04:46:36 +0300

    navbar dimensions and animation

---
+++ javaScript/navBar.js
@@ -0,0 +1,9 @@
+
+const menu = document.querySelector('#mobile-menu');
+const menuLinks=documents.querySelector('.nav-menu');
+
+menu.addEventListener('click', function(){
+    menu.classList.toggle('is-active');
+    manu.classList.toggle('active');
+})
+
