9d33856cf0c4a47bf358b9573bd6cea81ddd89b6
Alexandru Lucaci B2 <lucacialexandru18@gmail.com>
2022-06-22 10:24:06 +0300

    init model

--- models/Model.php
+++ models/Model.php
@@ -1,6 +1,25 @@
 <?php
 
+    abstract class Model{
 
+        public $connection;
+        private $querry;
+
+
+        public function __constract(){
+            $this->connection = Database::getConn();
+
+
+        }
+        public function setQuerry($querry){
+            $this->querry = $querry;
+        }
+
+
+
+
+
+    }
 
 
 ?>
