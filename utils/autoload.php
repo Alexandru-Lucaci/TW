a166d9eeeb573f4f1a261203c9ed8d590055d978
Alexandru Lucaci B2 <lucacialexandru18@gmail.com>
2022-06-22 14:30:31 +0300

    Update autoload.php

--- utils/autoload.php
+++ utils/autoload.php
@@ -1,7 +1,23 @@
 <?php
 
     function autoload($clasa){
-        if()
+        if(file_exists(HOME . DS . 'utils' . DS . strtolower($clasa) . '.php'));
+        {
+            require_once HOME . DS . 'utils' . DS . strtolower($clasa) . '.php';
+        }
+        if(file_exists(HOME . DS . 'models' . DS . strtolower($clasa) . '.php'));
+        {
+            require_once HOME . DS . 'models' . DS . strtolower($clasa) . '.php';
+        }
+        if(file_exists(HOME . DS . 'controllers' . DS . strtolower($clasa) . '.php'));
+        {
+            require_once HOME . DS . 'controllers' . DS . strtolower($clasa) . '.php';
+        }
+        if(file_exists(HOME . DS . 'controllers' . DS . strtolower($clasa) . '.php'));
+        {
+            require_once HOME . DS . 'controllers' . DS . strtolower($clasa) . '.php';
+        }
     }
+    spl_autoload_register('autoload');
 
 ?>
