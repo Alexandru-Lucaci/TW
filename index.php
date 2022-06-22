a1c04f1e47976f707aecfe9763037ad72a4d5a78
Alexandru Lucaci B2 <lucacialexandru18@gmail.com>
2022-06-22 09:18:33 +0300

    init datas for starting mvc

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
 
-    
+    session_start();
+    define('HOME', dirname(__FILE__));
+    define('DS', DIRECTORY_SEPARATOR);
 
+
+    // echo HOME;
+    // echo DS;
+    // echo $_SERVER['PHP_SELF']; -- dirname face sa fie slash-urile cum trebuie
+
+
+    require_once HOME . DS . 'config.php';
+    
 ?>
