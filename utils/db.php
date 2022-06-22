4ecc62d49037767c3d606601e1919a7594d4bbec
Alexandru Lucaci B2 <lucacialexandru18@gmail.com>
2022-06-22 08:59:42 +0300

    Finally :100:

--- utils/db.php
+++ utils/db.php
@@ -1,27 +1,21 @@
 <?php
-    
-
     class Database {
-
         private static $connection;
         public static $ok;
         // singleton class -needed for connection
         public static function getConn(){
-
             $options =[
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
                 PDO::ATTR_PERSISTENT => true
             ];
-            self::$connection = new PDO('oci.dbname=localhost/XE', 'TW_BD_ORACLE', 'TW_BD_ORACLE');
-            if(!empty($connection)){
-                $ok = ' working';
+            self::$connection = new PDO('oci:dbname=localhost/XE', 'TW_BD_ORACLE', 'TW_BD_ORACLE',$options);
+            if(!is_null(self::$connection)){
+                self::$ok = 'working';
                 return self::$connection;
             }
-
-
             else{
                 echo 'Something went wrong. BD-CONNECTION TO THE DATABASE';
-                $ok='notworking';
+                self::$ok='notworking';
             }
         }
 
