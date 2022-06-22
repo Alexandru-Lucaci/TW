bd8e7c081cbbd7c4c8c3eab02a8604a513fff6d1
Alexandru Lucaci B2 <lucacialexandru18@gmail.com>
2022-06-22 09:36:51 +0300

    not really working how i wanted

--- controllers/Controller.php
+++ controllers/Controller.php
@@ -6,11 +6,28 @@
         // controllerul ar trebui să cunoască atât view-ul cât și modelul
         public $model;
         public $view;
+        public $thisClassName = get_class($this);
 
-        public function __construct()
-        {
-            
-        }
     }
 
 ?>
+<?php
+    class verificare extends Controller{
+        public function getClass(){
+            echo $this->thisClassName;
+            return $this->thisClassName;
+        }
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>test</title>
+</head>
+<body>
+    <?php str_replace('Alex', 'Alexandru', 'Alex lucaci');?>
+</body>
+</html>
