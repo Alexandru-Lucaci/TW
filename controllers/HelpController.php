53779e328aa452aec67b999dd915dbacb4c03869
Alexandru Lucaci B2 <lucacialexandru18@gmail.com>
2022-06-22 20:34:32 +0300

    Create HelpController.php

---
+++ controllers/HelpController.php
@@ -0,0 +1,13 @@
+<?php
+
+    class HomeController extends Controller{
+
+        public function show(){
+            echo "should show";
+            $presentation = $this->view->show();
+            
+            echo $presentation;
+        }
+    }
+
+?>
