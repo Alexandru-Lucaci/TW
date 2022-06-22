60308ca530973c92159463f395551a10896a7b54
Alexandru Lucaci B2 <lucacialexandru18@gmail.com>
2022-06-22 09:07:33 +0300

    WORKING just fine :+1:

--- config.php
+++ config.php
@@ -3,7 +3,10 @@
     // dates needed for connection to the database
     // 'oci:dbname=localhost/XE', 'TW_BD_ORACLE', 'TW_BD_ORACLE'
     define('HOST','oci:dbname=localhost/XE');
-    define('usname','TW_BD_ORACLE');
-    define('uspass','TW_BD_ORACLE');
+    define('USNAME','TW_BD_ORACLE');
+    define('USPASS','TW_BD_ORACLE');
+    define('OPTIONS' , array(
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_PERSISTENT => true));
 
 ?>
--- utils/db.php
+++ utils/db.php
@@ -1,14 +1,12 @@
 <?php
+    require '../config.php';
     class Database {
         private static $connection;
         public static $ok;
         // singleton class -needed for connection
         public static function getConn(){
-            $options =[
-                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-                PDO::ATTR_PERSISTENT => true
-            ];
-            self::$connection = new PDO('oci:dbname=localhost/XE', 'TW_BD_ORACLE', 'TW_BD_ORACLE',$options);
+
+            self::$connection = new PDO(HOST, USNAME, USPASS ,OPTIONS);
             if(!is_null(self::$connection)){
                 self::$ok = 'working';
                 return self::$connection;
@@ -22,3 +20,19 @@
     }
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Check if it worked</h1>
+    <?php
+        Database::getConn();
+        echo Database::$ok;
+    ?>
+</body>
+</html>
