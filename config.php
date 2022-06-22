3dedf6fec4ddcad8daafd403648048d313c189eb
Alexandru Lucaci B2 <lucacialexandru18@gmail.com>
2022-06-22 09:04:26 +0300

    make it prettier

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+    // dates needed for connection to the database
+    // 'oci:dbname=localhost/XE', 'TW_BD_ORACLE', 'TW_BD_ORACLE'
+    define('HOST','oci:dbname=localhost/XE');
+    define('usname','TW_BD_ORACLE');
+    define('uspass','TW_BD_ORACLE');
+
+?>
