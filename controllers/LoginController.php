bf225e737978dec76e25d05e608b14fb39f8ae75
Alexandru Lucaci B2 <lucacialexandru18@gmail.com>
2022-06-23 16:01:02 +0300

    update login controller, something is not working

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -9,6 +9,19 @@
             echo $presentation;
         }
         public function doRegOrLog(){
+            if(filter_has_var(INPUT_POST,'submit')){
+                if(!empty($_POST['function'])){
+                    $functie = $_POST['function'];
+                    $class = new $this->model;
+                    $continut = $class->$functie();
+
+                }
+            }else{
+                echo 'something is not good';
+            }
+
+            $presentation= $this->view->show();
+            echo $presentation;
             
         }
     }
