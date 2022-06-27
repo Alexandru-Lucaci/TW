578d93da78175f82c7e2e5478505109007caf8e1
Alexandru Lucaci B2 <lucacialexandru18@gmail.com>
2022-06-27 12:53:26 +0300

    insert element into animal databes init

--- models/ImportModel.php
+++ models/ImportModel.php
@@ -6,6 +6,44 @@
             parent::__construct();
         
         }
+
+        public function insert_animal_info(){
+            // primele trei campuri sunt obligatorii
+            // input_option
+            // DENUMIRE_POPULARA
+            // DENUMIRE_STINTIFICA
+            // MINI_DESCRIERE
+            if(isset($_POST['input_option']) && !empty($_POST['input_option'])) {
+
+                
+                $tipInsert = $_POST['input_option'];
+                if($tipInsert == 'direct'){
+                    //inserez prin input direct 
+                    if(!( isset($_POST['DENUMIRE_POPULARA']) && isset($_POST['DENUMIRE_STINTIFICA'])&& isset($_POST['MINI_DESCRIERE']) && 
+                         !empty($_POST['DENUMIRE_POPULARA']) && !empty($_POST['DENUMIRE_STINTIFICA'])&& !empty($_POST['MINI_DESCRIERE']))){
+        
+                        echo 'variabile care nu au fost completate';
+                        return 'variabile care nu au fost completate';
+                    }
+                    $valori = array();
+                    $valori['DENUMIRE_POPULARA'] = $_POST['DENUMIRE_POPULARA'];
+                    $valori['DENUMIRE_STINTIFICA'] = $_POST['DENUMIRE_STINTIFICA'];
+                    $
+                }
+                else
+                {
+                    echo 'prin fisier';
+                    // inserez prin fisier 
+                }
+
+            }
+            else{
+                // cam imposibil
+                echo ' ceva nu e ok';
+                return 'ceva nu e ok input_option nu e sestat';
+            }
+
+        }
     
     }
 ?>
